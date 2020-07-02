ab2c50a1f6714b6a8225213aa370d494f58ed8b6
takanuva15 <takanuva15@users.noreply.github.com>
2020-07-02 00:15:18 -0400

    #17: Moved custom UI dsl methods to a new kt file. Cleaned up createEditor method and removed old files that aren't needed anymore

--- src/main/java/de/nordgedanken/auto_hotkey/run_configurations/ui/AhkRunConfigSettingsEditor.kt
+++ src/main/java/de/nordgedanken/auto_hotkey/run_configurations/ui/AhkRunConfigSettingsEditor.kt
@@ -1,23 +1,22 @@
 package de.nordgedanken.auto_hotkey.run_configurations.ui
 
-import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
-import com.intellij.ui.components.JBTabbedPane
 import com.intellij.ui.components.fields.ExpandableTextField
 import com.intellij.ui.layout.CCFlags
 import com.intellij.ui.layout.panel
 import de.nordgedanken.auto_hotkey.localization.AhkBundle
 import de.nordgedanken.auto_hotkey.run_configurations.core.AhkRunConfig
-import java.awt.Insets
-import javax.swing.BorderFactory
 import javax.swing.JComponent
-import javax.swing.border.CompoundBorder
-import javax.swing.border.EmptyBorder
 
-
+/**
+ * Contains the settings that are shown when editing a run configuration.
+ *
+ * (If you're new to Kotlin, createEditor() will seem weird but it's just using
+ * the UI DSL format to construct the UI instead of a .form file)
+ */
 class AhkRunConfigSettingsEditor(private val project: Project) : SettingsEditor<AhkRunConfig>() {
     private val pathToScriptTextField: TextFieldWithBrowseButton = TextFieldWithBrowseButton().apply {
         addBrowseFolderListener(AhkBundle.msg("runconfig.configtab.scriptpath.filechooser.title"),
@@ -38,37 +37,18 @@ class AhkRunConfigSettingsEditor(private val project: Project) : SettingsEditor<
         s.arguments = argumentsTextField.text
     }
 
-    override fun createEditor(): JComponent {
-        val runConfigSettingsPanel = panel {
-            row(AhkBundle.msg("runconfig.configtab.scriptpath.label")) { pathToScriptTextField() }
-            row(AhkBundle.msg("runconfig.configtab.scriptargs.label")) { argumentsTextField(growX, pushX) }
-            row(AhkBundle.msg("runconfig.configtab.ahksdk.label")) {
-                label(AhkBundle.msg("runconfig.configtab.ahksdk.info"))
-            }
-            row {
-                label(AhkBundle.msg("runconfig.general.info.label"))
-            }
-        }.apply {
-            val lineBorder = BorderFactory.createLineBorder(EditorColorsManager.getInstance().globalScheme.defaultForeground)
-            val margin = EmptyBorder(10, 10, 10, 10)
-            border = CompoundBorder(lineBorder, margin)
-        }
-        val runConfigPane = JBTabbedPane().apply {
-            tabComponentInsets = Insets(0,0,0,0)
-            addTab(AhkBundle.msg("runconfig.configtab.label"), runConfigSettingsPanel)
-
-        }
-        val rootPane = panel {
-            row {
-                runConfigPane(CCFlags.growX, pushX)
+    override fun createEditor(): JComponent = panel {
+        row {
+            tabbedPane(CCFlags.growX, CCFlags.pushX) {
+                outlinedTab(AhkBundle.msg("runconfig.configtab.label")) {
+                    row(AhkBundle.msg("runconfig.configtab.scriptpath.label")) { pathToScriptTextField() }
+                    row(AhkBundle.msg("runconfig.configtab.scriptargs.label")) { argumentsTextField(growX, pushX) }
+                    row(AhkBundle.msg("runconfig.configtab.ahksdk.label")) {
+                        label(AhkBundle.msg("runconfig.configtab.ahksdk.info"))
+                    }
+                    noteRow(AhkBundle.msg("runconfig.general.info.label"))
+                }
             }
         }
-        return rootPane
     }
-
-//    private fun LayoutBuilder.labeledRow(labelText: String, component: JComponent, init: Row.() -> Unit) {
-//        val label = Label(labelText)
-//        label.labelFor = component
-//        row(label) { init() }
-//    }
 }
